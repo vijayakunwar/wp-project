40bbad4535e348232524aef4f5f8f41f375978b8
vijayakunwar <kunwar.vijaya@gmail.com>
2024-12-20 00:28:47 +1000

    add post loop to the archive and front page

--- wp-content/themes/mycustomtheme/front-page.php
+++ wp-content/themes/mycustomtheme/front-page.php
@@ -1,6 +1,16 @@
 executing: front-page.php
 
-<?php get_header(); ?>  
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1> <?php the_title(); ?> </h1>
+        <p> <?php the_content(); ?> </p>
+    <?php endwhile; ?>
+<?php else : ?>
+    <p> <?php _e('Sorry, no posts matched your criteria.'); ?> </p> <!-- // it will redirect to 404 page if no post were found -->
+   
+    
+<?php endif; ?>
 
 
 
--- wp-content/themes/mycustomtheme/single-post.php
+++ wp-content/themes/mycustomtheme/single-post.php
@@ -1,17 +1,16 @@
-executing: archive.php
+executing:  single-post.php 
 
-<?php get_header(); ?>  
+<?php get_header(); ?>
 
-<!-- DISPLAY all POST BY CATEGORY -->
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <h1> <?php the_title(); ?> </h1>
-        <p> <?php the_excerpt(); ?> </p>
-        <a href="<?php the_permalink(); ?>">Read More</a>
+        <p> <?php the_content(); ?> </p>
     <?php endwhile; ?>
 <?php else : ?>
     <p> <?php _e('Sorry, no posts matched your criteria.'); ?> </p> <!-- // it will redirect to 404 page if no post were found -->
+   
+    
 <?php endif; ?>
-
-
+ 
 
 <?php get_footer(); ?>
